221fe05035d2cdf726109b36e4d9e37f3e3c37ad
elazrev <129072537+elazrev@users.noreply.github.com>
2023-09-19 12:12:44 +0300

    02_module_task

---
+++ part_09_modules/class_tasks/02_module_task/calculator.py
@@ -0,0 +1,11 @@
+def plus(a, b):
+    return f"{a} + {b} = {a + b}"
+
+def minus(a, b):
+    return f"{a} - {b} = {a - b}"
+
+def multi(a, b):
+    return f"{a} + {b} = {a + b}"
+
+def divid(a, b):
+    return f"{a} + {b} = {a + b}"
